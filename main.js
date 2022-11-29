f5d59537949a255df50a1b7ba451a88d09f02c4c
ortegi <maria3108x@gmail.com>
2022-11-29 12:36:15 +0100

    2nd commit

--- main.js
+++ main.js
@@ -26,19 +26,26 @@ async function getAdress(lat,lon){
         const response = await fetch(`https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lon}&format=json&zoom=18&addressdetails=1`)
         if (response.ok){
             const jsonResponse = await response.json()
-            let town = jsonResponse.address.town
-            let code = jsonResponse.address.postcode
-            let address = jsonResponse.address.residential
+           // let town = jsonResponse.address.town
+            //let code = jsonResponse.address.postcode
+            let address = jsonResponse.display_name
+
 
             console.log(jsonResponse)
-            showAdress(address, town, code)
+            showAdress(address, jsonResponse)
         }
     }catch(error){
         console.log(error)
     }
 }
 
-function showAdress(address, town, code){
+function showAdress(address, jsonResponse){
     let x = document.querySelector('#adress')
-    x.value = `${address}, ${town}, ${code}`
+    x.value = `${address}`
+    let y = document.querySelector('#test')
+    y.innerHTML = `<p>${jsonResponse.address.residential}, ${jsonResponse.address.residential} 
+    ${jsonResponse.address.suburb}
+    ${jsonResponse.address.postcode}
+  
+    </p>`
 }
